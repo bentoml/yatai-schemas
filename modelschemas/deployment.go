424377b0458ca12e6783d9fe53076e1ae951319a
yetone <yetoneful@gmail.com>
2022-09-22 10:52:13 +0000

    feat: add image-building status to deployment

--- modelschemas/deployment.go
+++ modelschemas/deployment.go
@@ -3,14 +3,15 @@ package modelschemas
 type DeploymentStatus string
 
 const (
-	DeploymentStatusUnknown     DeploymentStatus = "unknown"
-	DeploymentStatusNonDeployed DeploymentStatus = "non-deployed"
-	DeploymentStatusRunning     DeploymentStatus = "running"
-	DeploymentStatusUnhealthy   DeploymentStatus = "unhealthy"
-	DeploymentStatusFailed      DeploymentStatus = "failed"
-	DeploymentStatusDeploying   DeploymentStatus = "deploying"
-	DeploymentStatusTerminating DeploymentStatus = "terminating"
-	DeploymentStatusTerminated  DeploymentStatus = "terminated"
+	DeploymentStatusUnknown       DeploymentStatus = "unknown"
+	DeploymentStatusNonDeployed   DeploymentStatus = "non-deployed"
+	DeploymentStatusRunning       DeploymentStatus = "running"
+	DeploymentStatusUnhealthy     DeploymentStatus = "unhealthy"
+	DeploymentStatusFailed        DeploymentStatus = "failed"
+	DeploymentStatusDeploying     DeploymentStatus = "deploying"
+	DeploymentStatusTerminating   DeploymentStatus = "terminating"
+	DeploymentStatusTerminated    DeploymentStatus = "terminated"
+	DeploymentStatusImageBuilding DeploymentStatus = "image-building"
 )
 
 func (d DeploymentStatus) Ptr() *DeploymentStatus {
